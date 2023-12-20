68513fef82c6c2e55d8f980d1e4a8930b93624e6
Witold Wegner <witold.wegner@74bit.de>
2023-12-20 15:07:14 +0100

    set back to original remote

--- pushall.sh
+++ pushall.sh
@@ -4,23 +4,30 @@
 #
 remote=$1
 if [ -z "$remote" ]; then
-  echo "Please provide a remote name"
-  exit 1
+    echo "Please provide a remote name"
+    exit 1
 fi
 # Get the list of all local branches
 branches=$(git branch --format='%(refname:short)')
 
 # Iterate over each branch
 for branch in $branches; do
-
-upstream=$(git rev-parse --abbrev-ref "$branch@{upstream}" 2>/dev/null)
-
-if git ls-remote --exit-code --heads "$remote_name" "$upstream" >/dev/null 2>&1; then
-  echo "upstream present - pushing $branch to $remote"
-  git push
-else
-    echo "no upstream - setting upstream for $branch in $remote"    
-    echo "git push -u $remote \"$branch\""
-    git push -u $remote "$branch"
-fi
+    
+    original_remote=$(git config branch."$branch".remote)
+        
+    upstream=$(git rev-parse --abbrev-ref "$branch@{upstream}" 2>/dev/null)
+    
+    if git ls-remote --exit-code --heads "$remote_name" "$upstream" >/dev/null 2>&1; then
+        echo "upstream present - pushing $branch to $remote"
+        git push
+    else
+        echo "no upstream - setting upstream for $branch in $remote"
+        echo "git push -u $remote \"$branch\""
+        git push -u $remote "$branch"
+    fi
+    
+    # Set the branch back to the original remote
+    echo "git branch --set-upstream-to=\"$original_remote/$branch\" \"$branch\""
+    git branch --set-upstream-to="$original_remote/$branch" "$branch"
 done
+
